0d58ed6181ae174425e4427ee6105b3eb3a5fc4c
Johan Egneblad <johan@egneblad.se>
2018-10-31 22:57:32 +0100

    New improved layout

--- keyboards/kbd4x/keymaps/johan/keymap.c
+++ keyboards/kbd4x/keymaps/johan/keymap.c
@@ -27,32 +27,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_planck_mit(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENTER,
-    KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLASH,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLASH, KC_QUOT,
     KC_LCTL, KC_LGUI, KC_LALT, KC_RALT, MO(1),   LT(3, KC_SPACE),  MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT
   ),
 
 // LOWER (LEFT)
   [1] = LAYOUT_planck_mit(
-    KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_INS,
-    ___, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, ___, KC_QUOT, KC_DQT, KC_PLUS, KC_LBRC, KC_RBRC, KC_EQL,
-    ___, BL_TOGG, BL_STEP, ___, ___, ___,  ___, KC_UNDS, KC_MINUS, KC_LCBR, KC_RCBR, KC_MPLY,
-    ___, ___, ___, ___, ___, KC_BSPC,  ___, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    KC_GRAVE, KC_1,    KC_2,   KC_3, KC_4, KC_5, KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     ___,
+    KC_TILDE, RGB_TOG, ___,    ___,  ___,  ___,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_MINUS, KC_EQL,
+    ___,      KC_BSLS, KC_DEL, ___,  ___,  ___,  ___,     ___,    ___,      ___,     ___,       KC_INS,
+    ___,      ___,     ___,    ___,  ___,  KC_SPACE,      ___,    KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END
   ),
 
 // UPPER (RIGHT)
   [2] = LAYOUT_planck_mit(
-    KC_TILDE, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-    ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, RESET,
-    ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, KC_MUTE,
-    ___, ___, ___, ___, ___, KC_DEL, ___, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
+    ___, KC_F1, KC_F2,  KC_F3,  KC_F4,  KC_F5, KC_F6, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+    ___, KC_F5, KC_F6,  KC_F7,  KC_F8,  ___,   ___,   ___,     ___,     ___,     ___,     RESET,
+    ___, KC_F9, KC_F10, KC_F11, KC_F12, ___,   ___,   ___,     ___,     ___,     KC_MUTE, KC_MPLY,
+    ___, ___,   ___,    ___,    ___,    KC_SPACE,     ___,     KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
   ),
 
 // FUNCTION (SPACE)
   [3] = LAYOUT_planck_mit(
-    KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-    ___, ___, KC_PSCR, ___, ___, ___, ___, ___, ___, ___, ___, ___,
-    ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___,
-    ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___
+    ___, ___, ___,     ___, ___, ___, KC_LPRN, KC_RPRN, ___, KC_MINUS, KC_EQL, ___,
+    ___, ___, KC_PSCR, ___, ___, ___, KC_LBRC, KC_RBRC, ___, ___,      ___,    ___,
+    ___, ___, KC_DEL,  ___, ___, ___, KC_LCBR, KC_RCBR, ___, ___,      ___,    ___,
+    ___, ___, ___,     ___, ___, KC_SPACE,     ___,     ___, ___,      ___,    ___
   ),
 };
 
