abab90844cee8f09a7233ba447bf03f3d294ac9d
Johan Egneblad <johan@egneblad.se>
2019-03-20 22:35:21 +0100

    Johan3 for both keyboards

--- keyboards/kbd4x/keymaps/johan3/keymap.c
+++ keyboards/kbd4x/keymaps/johan3/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     GRAVE_ESC,KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSPC,
     KC_TAB,   KC_A,     KC_S,     KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_ENTER,
     KC_LSPO,  KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLASH, KC_QUOT,
-    KC_LCTL,  KC_LGUI,  KC_LALT,  KC_MINUS,TO(1),   KC_SPACE,          MO(2),   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT
+    KC_LCTL,  KC_LGUI,  KC_LALT,  KC_MINUS,MO(1),   KC_SPACE,          MO(2),   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT
   ),
 
 // NUMERIC (LEFT)
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [2] = LAYOUT_planck_mit(
     KC_F1,    KC_F2,    KC_F3,    KC_F4,   KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,   KC_F12,
     ___,      RGB_TOG,  KC_PSCR,  KC_INS,  ___,     ___,      ___,     ___,     ___,      ___,      ___,      RESET,
-    ___,      KC_HOME,  KC_DEL,   KC_END,  ___,     ___,      ___,     ___,     TO(0),    TO(3),    KC_MUTE,  KC_MPLY,
+    ___,      KC_HOME,  KC_DEL,   KC_END,  ___,     ___,      ___,     TO(0),   TO(1),    TO(3),    KC_MUTE,  KC_MPLY,
     ___,      ___,      ___,      ___,     KC_PGDN, KC_PGUP,           ___,     KC_MPRV,  KC_VOLD,  KC_VOLU,  KC_MNXT
   ),
 
