5f12000fd5a86393941dc85b7f552505b1af4747
Johan Egneblad <johan@egneblad.se>
2019-04-09 22:41:25 +0200

    Better layer handling for swedish layer and no ESC_GRAVE

--- keyboards/jj40/keymaps/johan3/keymap.c
+++ keyboards/jj40/keymaps/johan3/keymap.c
@@ -30,9 +30,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12( \
-  GRAVE_ESC,KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENTER, \
-  KC_LSPO,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, \
+  KC_ESC,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,       KC_I,    KC_O,    KC_P,    KC_BSPC, \
+  KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,       KC_K,    KC_L,    KC_SCLN, KC_ENTER, \
+  KC_LSPO,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, \
   KC_LCTL,  KC_LGUI, KC_LALT, KC_MINUS,MO(_LOWER),SPACE, SPACE,   MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12( \
-    GRAVE_ESC, KC_1,     KC_2,     KC_3,    KC_4,        KC_5,     KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     ___, \
+    KC_GRAVE,  KC_1,     KC_2,     KC_3,    KC_4,        KC_5,     KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     ___, \
     KC_TILDE,  KC_LCBR,  KC_RCBR,  KC_LBRC, KC_RBRC,     ___,      ___,     ___,     ___,      ___,      ___,      KC_EQL, \
     ___,       KC_BSLS,  KC_PIPE,  KC_UNDS, ___,         ___,      ___,     ___,     ___,      ___,      ___,      KC_RPRN, \
     ___,       ___,      ___,      ___,     ___,         ___,      ___,     ___,     ___,      ___,      ___,      ___ \
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_ortho_4x12( \
     KC_F1,    KC_F2,    KC_F3,    KC_F4,   KC_F5,   KC_F6,    KC_F7,   KC_F8,       KC_F9,       KC_F10,   KC_F11,   KC_F12, \
     ___,      RGB_TOG,  KC_PSCR,  KC_INS,  ___,     ___,      ___,     ___,         ___,         ___,      ___,      RESET, \
-    ___,      KC_HOME,  KC_DEL,   KC_END,  ___,     ___,      ___,     TO(_QWERTY), TO(_LOWER),  TO(_SWE), KC_MUTE,  KC_MPLY, \
+    ___,      KC_HOME,  KC_DEL,   KC_END,  ___,     ___,      ___,     ___,         ___,         TO(_SWE), KC_MUTE,  KC_MPLY, \
     ___,      ___,      ___,      ___,     KC_PGDN, KC_PGUP,  KC_ASTR, ___,         KC_MPRV,     KC_VOLD,  KC_VOLU,  KC_MNXT \
 ),
 
@@ -85,10 +85,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_SWE] = LAYOUT_ortho_4x12( \
-    ___,      ___,      ___,      ___,     ___,     ___,       ___,     ___,    ___,      ___,      ___,        ___, \
-    ___,      ___,      ___,      ___,     ___,     ___,       ___,     ___,    ___,      ___,      RALT(KC_W), ___, \
-    ___,      ___,      ___,      ___,     ___,     ___,       ___,     ___,    ___,      ___,      RALT(KC_Q), RALT(KC_P), \
-    ___,      ___,      ___,      ___,     TO(0),   ___,       ___,     ___,    ___,      ___,      ___,        ___ \
+    TO(_QWERTY), ___,      ___,      ___,     ___,     ___,       ___,     ___,    ___,      ___,      ___,        ___, \
+    ___,         ___,      ___,      ___,     ___,     ___,       ___,     ___,    ___,      ___,      RALT(KC_W), ___, \
+    ___,         ___,      ___,      ___,     ___,     ___,       ___,     ___,    ___,      ___,      RALT(KC_P), RALT(KC_Q), \
+    ___,         ___,      ___,      ___,     ___,     ___,       ___,     ___,    ___,      ___,      ___,        ___ \
 )
 
 /* Adjust
--- keyboards/kbd4x/keymaps/johan3/keymap.c
+++ keyboards/kbd4x/keymaps/johan3/keymap.c
@@ -26,7 +26,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // STANDARD
   [0] = LAYOUT_planck_mit(
-    GRAVE_ESC,KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSPC,
+    KC_ESC,   KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSPC,
     KC_TAB,   KC_A,     KC_S,     KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_ENTER,
     KC_LSPO,  KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLASH, KC_QUOT,
     KC_LCTL,  KC_LGUI,  KC_LALT,  KC_MINUS,MO(1),   KC_SPACE,          MO(2),   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT
@@ -34,26 +34,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // NUMERIC (LEFT)
   [1] = LAYOUT_planck_mit(
-    GRAVE_ESC,KC_1,     KC_2,     KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_BSPC,
+    KC_GRAVE, KC_1,     KC_2,     KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_BSPC,
     KC_TILDE, KC_LCBR,  KC_RCBR,  KC_LBRC, KC_RBRC, ___,      ___,     ___,     ___,      ___,      ___,      KC_EQL,
     ___,      KC_BSLS,  KC_PIPE,  KC_UNDS, ___,     ___,      ___,     ___,     ___,      ___,      ___,      KC_RPRN,
-    ___,      ___,      ___,      ___,     TO(0),   ___,               ___,     ___,      ___,      ___,      ___
+    ___,      ___,      ___,      ___,     ___,     ___,               ___,     ___,      ___,      ___,      ___
   ),
 
 // FUNCTION (RIGHT)
   [2] = LAYOUT_planck_mit(
     KC_F1,    KC_F2,    KC_F3,    KC_F4,   KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,   KC_F12,
     ___,      RGB_TOG,  KC_PSCR,  KC_INS,  ___,     ___,      ___,     ___,     ___,      ___,      ___,      RESET,
-    ___,      KC_HOME,  KC_DEL,   KC_END,  ___,     ___,      ___,     TO(0),   TO(1),    TO(3),    KC_MUTE,  KC_MPLY,
+    ___,      KC_HOME,  KC_DEL,   KC_END,  ___,     ___,      ___,     ___,     ___,      TO(3),    KC_MUTE,  KC_MPLY,
     ___,      ___,      ___,      ___,     KC_PGDN, KC_PGUP,           ___,     KC_MPRV,  KC_VOLD,  KC_VOLU,  KC_MNXT
   ),
 
 // SWEDISH
   [3] = LAYOUT_planck_mit(
-    ___,      ___,      ___,      ___,     ___,     ___,       ___,     ___,    ___,      ___,      ___,        ___,
+    TO(0),    ___,      ___,      ___,     ___,     ___,       ___,     ___,    ___,      ___,      ___,        ___,
     ___,      ___,      ___,      ___,     ___,     ___,       ___,     ___,    ___,      ___,      RALT(KC_W), ___,
-    ___,      ___,      ___,      ___,     ___,     ___,       ___,     ___,    ___,      ___,      RALT(KC_Q), RALT(KC_P),
-    ___,      ___,      ___,      ___,     TO(0),   ___,                ___,    ___,      ___,      ___,        ___
+    ___,      ___,      ___,      ___,     ___,     ___,       ___,     ___,    ___,      ___,      RALT(KC_P), RALT(KC_Q),
+    ___,      ___,      ___,      ___,     ___,      ___,               ___,    ___,      ___,      ___,        ___
   ),
 };
 
