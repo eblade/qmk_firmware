2d5a584ff163585a87cb759d0be57cfa5553b3cc
Johan Egneblad <johan@egneblad.se>
2025-05-19 09:27:16 +0200

    Fix some tapping

--- keyboards/40percentclub/gherkin/keymaps/johan/config.h
+++ keyboards/40percentclub/gherkin/keymaps/johan/config.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #define TAPPING_TERM 200
-#define IGNORE_MOD_TAP_INTERRUPT
+//#define IGNORE_MOD_TAP_INTERRUPT
 #define COMBO_COUNT 45
 #define COMBO_TERM 30
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
